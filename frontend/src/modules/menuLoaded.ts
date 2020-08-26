8ed1ec773b632aa7568ca0e474a4da8ad40c6b34
Jackie <brundtoe@outlook.dk>
2020-08-26 18:21:55 +0200

    frontend korrektur rettelser

--- frontend/src/modules/menuLoaded.ts
+++ frontend/src/modules/menuLoaded.ts
@@ -1,20 +1,18 @@
 import docClass from '../util/renderHtmlElement.js'
 
 export default function (menu: string, domElement: string) {
-  window.fetch(menu)
-    .then(function (response) {
-      if (response.ok) {
-        return response.text()
-      } else {
-        throw new Error('partial html er ikke indlæst')
-      }
-    })
-    .then(function (result) {
-      docClass.render(domElement, result)
-    })
-    .catch(function (err) {
-      console.log(err)
-      alert('Filen er ikke indlæst')
-    })
-
+    window.fetch(menu)
+        .then(function (response) {
+            if (response.ok) {
+                return response.text()
+            }
+            throw new Error('partial html er ikke indlæst')
+        })
+        .then(function (result) {
+            docClass.render(domElement, result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            alert('Filen er ikke indlæst')
+        })
 }
