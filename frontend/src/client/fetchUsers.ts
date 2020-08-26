6ca17173764a7df4447771000e78674bbffee73c
Jackie <brundtoe@outlook.dk>
2020-08-26 16:36:15 +0200

    tilføjet modulet config til brug ved valg af URI ved kald af backend

--- frontend/src/client/fetchUsers.ts
+++ frontend/src/client/fetchUsers.ts
@@ -1,10 +1,10 @@
 import fetchJson from '../modules/fetchJson.js';
 import handleUsers from '../modules/formaterUsers.js';
+import config from '../util/config.js'
 
 document.addEventListener('DOMContentLoaded', function(e){
 
-  const url = `${window.location.origin}/bookstore/users/sample/12`
-  //const url = `http://localhost:3300/bookstore/users/sample/12`;
+  const url = `${config.host_uri}/bookstore/users/sample/12`
   fetchJson(url, 'data', handleUsers);
 
 })
