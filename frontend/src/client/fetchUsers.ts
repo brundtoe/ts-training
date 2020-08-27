8319fdfe2565fb4fb98f0f7ca572e16a551eeaad
Jackie <brundtoe@outlook.dk>
2020-08-27 17:39:54 +0200

    refaktoreret fetchJson anvender modellen fra awaitBackend, som dermed udgår

--- frontend/src/client/fetchUsers.ts
+++ frontend/src/client/fetchUsers.ts
@@ -1,10 +1,16 @@
-import fetchJson from '../modules/fetchJson.js';
-import handleUsers from '../modules/formaterUsers.js';
+import {fetchJson} from '../modules/fetchJson.js'
+import formaterUsers from '../modules/formaterUsers.js';
 import config from '../util/config.js'
+import docClass from "../util/renderHtmlElement.js";
 
 document.addEventListener('DOMContentLoaded', function(e){
 
   const url = `${config.host_uri}/bookstore/users/sample/12`
-  fetchJson(url, 'data', handleUsers);
-
+  fetchJson(url).then((data) => {
+    const list = formaterUsers(data)
+    docClass.render('data',list)
+    console.log('hentet og indsat data på siden ')
+  }).catch((err) => {
+    console.log(err)
+  })
 })
--- frontend/src/modules/fetchJson.ts
+++ frontend/src/modules/fetchJson.ts
@@ -1,22 +1,11 @@
-import docClass from "../util/renderHtmlElement.js";
-
-export default (url: string, domElement: string, handleResponse: any) => {
-    fetch(url,{
+export async function fetchJson(url: string) {
+    const response = await window.fetch(url,{
         headers: {'Accept': 'application/json'}
     })
-        .then((response) => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                return Promise.reject(response)
-            }
-        })
-        .then(function (data) {
-            const list = handleResponse(data)
-            docClass.render(domElement,list)
-        })
-        .catch((error) => {
-            const res = `<tr><td>${error.statusText}</td></tr><tr><td>${error.url}</td></tr>`
-            docClass.render(domElement,res)
-        })
+    const data = await response.json()
+    return data
 }
+
+
+
+
