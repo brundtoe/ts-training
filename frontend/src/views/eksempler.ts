f5d895736ac79a73412213404be6221c0b27d4b8
Jackie <brundtoe@outlook.dk>
2020-08-27 17:59:49 +0200

    frontend: eksempel 5 anvender request for backend authors sample i json format

--- frontend/src/views/eksempler.ts
+++ frontend/src/views/eksempler.ts
@@ -1,9 +1,10 @@
 export const authorsView: string = `<p>Welcome til <strong>{{message}}</strong></p>
-            <table id="authors" class="table table-sm">
-                {{#data}}
-                    <tr>
-                        <td>{{fullname this}}</td>
-                        <td>{{this.mail}}</td>
-                    </tr>
-                {{/data}}
-            </table> `
+<table id="authors" class="table table-sm">
+    {{#data}}
+        <tr>
+        <td>{{this.id}}</td>
+        <td>{{fullname this}}</td>
+        <td>{{this.mail}}</td>
+        </tr>
+    {{/data}}
+</table> `
