767d0bf7721960647880bac7061cb84785246120
Jackie <brundtoe@outlook.dk>
2020-08-27 10:20:19 +0200

    tilføjet handlebars eksempel 3 med es6 modul

---
+++ frontend/src/client/fetchAuhors.ts
@@ -0,0 +1,39 @@
+import docClass from '../util/renderHtmlElement.js'
+
+type authorType = {
+    firstname: string,
+    lastname: string,
+    mail: string
+}
+
+const authors = [
+    {
+        author:
+            {
+                firstname: 'Jens',
+                lastname: 'Andersen',
+                mail: 'jens@anymail.com'
+            }
+    },
+    {
+        author: {
+            firstname: 'Marie',
+            lastname: 'Hansen',
+            mail: 'marie@anymail.com'
+        }
+    }]
+
+export default function (handlebars: any, templateId: string, domElement: string) {
+
+    handlebars.registerHelper('fullname', function (author: authorType){
+        return `${author.firstname} ${author.lastname}`
+    })
+    const el = document.getElementById(templateId)
+    if (el) {
+        const source = el.innerHTML
+        const compiledTemplate = handlebars.compile(source)
+        const rendered = compiledTemplate({message: 'Handlebars template', authors})
+        docClass.render(domElement, rendered)
+
+    }
+}
