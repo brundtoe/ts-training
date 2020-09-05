ce0ca369bac33b7f28bcda8e18afb2f0a9b0c1f9
Jackie <brundtoe@outlook.dk>
2020-09-05 17:51:15 +0200

    justeret strukturen page-template view model

--- frontend/src/page-template/index.ts
+++ frontend/src/page-template/index.ts
@@ -11,22 +11,21 @@ import docElement from '../util/renderElement'
 document.addEventListener('DOMContentLoaded', (e) => {
 
     const users = bookstore.customers.filter(user => user.id <= 12)
-    const tmpl = `<% _.forEach(users, function(user){ %>
-        <tr> 
-            <td><%= user.id%></td>
-            <td><%= user.name%></td>
-            <td><%= user.city%></td>
-            <td><%= user.state%></td>
-            <td><%= user.mail%></td>
-        </tr>
-    <% }) %>`
+    const tmpl = `<table>
+        <% _.forEach(users, function(user){ %>
+            <tr> 
+                <td><%= user.id%></td>
+                <td><%= user.name%></td>
+                <td><%= user.city%></td>
+                <td><%= user.state%></td>
+                <td><%= user.mail%></td>
+            </tr>
+        <% }) %>
+    </table>`
 
     const compiled = _.template(tmpl)
     const html = compiled({users: users})
 
     docElement.renderHtml('data', `${html}`)
-    console.log('DOMContentLoaded', 'page-scrap')
-    $('#alert').on('click', () => {
-        alert('jQuery works!')
-    })
+    console.log('DOMContentLoaded', 'page-template')
 })
