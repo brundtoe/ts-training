8a9421470f2e6f8440f93a542ea9fc5d3440d1cb
Jackie <brundtoe@outlook.dk>
2021-03-22 09:34:04 +0100

    opdateret run configurations

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -3,7 +3,7 @@ export default {
     host_uri: window.location.origin
 }
 //
-/** Anvendes ved docker-einsteinish og docker-frontend, docker-backend
+/** Anvendes ved docker-frontend, docker-backend
  export default {
     host_uri: 'http://localhost:3300'
 }
