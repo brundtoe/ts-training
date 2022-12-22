566022be4c6fdd3ff5aabb549d9347d92069985a
Jackie <brundtoe@outlook.dk>
2022-12-22 13:45:36 +0100

    ændringer i ts og ts-jest compiler options som følge af ændringer i jest 28 (opgradering fra 27 til 29)

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,20 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  clearMocks: true,
-  roots: [
-    "./src"
-  ],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: './src/tsconfig.json'
-    }
-  }
-}
+    testEnvironment: 'node',
+    clearMocks: true,
+    roots: [
+        "./src"
+    ],
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": ["ts-jest",
+            {
+                useESM: true,
+            }
+        ]
+    },
+};
